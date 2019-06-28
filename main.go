8239b27b5d4f7d702d220c17bcfbbfb08a9df14c
Gustibimo <gstbimo@gmail.com>
2019-06-28 13:20:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -14,12 +14,11 @@ type formater func(io.Reader, []string) interface{}
 
 func main() {
 	var format string
-	flag.StringVar(&format, "format", "2d-array", "Output format to use (array, map, 2d-array)")
+	flag.StringVar(&format, "format", "map", "Output format to use (array, map, 2d-array)")
 	flag.Parse()
 
 	formats := map[string]formater{
-		"array":    toArray,
-		"2d-array": to2dArray,
+		"map": toMap,
 	}
 
 	f, ok := formats[format]
@@ -35,21 +34,28 @@ func main() {
 	enc.Encode(out)
 }
 
-func toArray(r io.Reader, args []string) interface{} {
+func toMap(r io.Reader, args []string) interface{} {
 	sc := bufio.NewScanner(r)
-	lines := make([]string, 0)
+	lines := make([]map[string]string, 0)
 	for sc.Scan() {
-		lines = append(lines, sc.Text())
-	}
-	return lines
-}
+		line := make(map[string]string)
+		fields := strings.Fields(sc.Text())
 
-func to2dArray(r io.Reader, args []string) interface{} {
-	sc := bufio.NewScanner(r)
-	lines := make([][]string, 0)
-	for sc.Scan() {
-		parts := strings.Fields(sc.Text())
-		lines = append(lines, parts)
+		for i, k := range args {
+			if len(fields) <= i {
+				break
+			}
+			// pass field that contain a dash
+			if k == "-" {
+				continue
+			}
+			line[k] = fields[i]
+		}
+
+		fmt.Print(line)
+
+		lines = append(lines, line)
 	}
+
 	return lines
 }
